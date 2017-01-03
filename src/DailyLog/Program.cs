c0cd5a5e2213c815dcb28c6f447fde0fa6e1f06a
rocco337 <roko.bobic@gmail.com>
2017-01-03 22:16:51 +0100

    add help text

--- src/DailyLog/Program.cs
+++ src/DailyLog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace DailyLog
 {
@@ -32,27 +33,47 @@ namespace DailyLog
 
                     loadLastTenLogs(fileReader);
                 }
+                else if (args[ii] == "-h")
+                {
+                    var helpText = new StringBuilder();
+
+                    helpText.AppendLine("     _ _");
+                    helpText.AppendLine("    | | |");
+                    helpText.AppendLine("  _ | | |      ___   ____");
+                    helpText.AppendLine(@" / || | |     / _ \ / _  |");
+                    helpText.AppendLine(" (_| | |____| |_| ( ( | |");
+                    helpText.AppendLine(@" \____|_______)___/ \_|| |");
+                    helpText.AppendLine("                   (_____|");
+                    helpText.AppendLine("");
+                    helpText.AppendLine("Log your daily thought using dLog. By specifying path to the file dLog is logging message to file. If file is on file sharing service like DropBox, you can use dLog on multiple devices.");
+                    helpText.AppendLine("-m \"simple message\"");                    
+                    helpText.AppendLine("-m \"message wth tags\" tag1,tag2,tag3");
+                    helpText.AppendLine("-f \"laod last messages\"");
+                    
+
+                    Console.Write(helpText.ToString());
+                }
             }
         }
 
-        private static void loadLastTenLogs(FileReader fileReader, int pageIndex=0)
-        {            
+        private static void loadLastTenLogs(FileReader fileReader, int pageIndex = 0)
+        {
             const int numOfLines = 10;
-            
+
             var logs = fileReader.ReadLines(pageIndex, numOfLines);
-            
+
             //if there are no logs returned exit recursion
-            if(logs.Length==0) return;
+            if (logs.Length == 0) return;
 
             foreach (var log in logs)
                 Console.WriteLine(log.TrimEnd().TrimEnd('-'));
-            
-            if(logs.Length<numOfLines) return;
-            
+
+            if (logs.Length < numOfLines) return;
+
             Console.WriteLine("n for next page");
             var nextPage = Console.ReadLine() == "n";
-            if(nextPage)
-                loadLastTenLogs(fileReader,++pageIndex);
+            if (nextPage)
+                loadLastTenLogs(fileReader, ++pageIndex);
         }
     }
 
